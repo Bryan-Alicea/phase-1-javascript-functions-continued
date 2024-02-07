06cb477663830a3cf7494aeeda5095d1b9f13745
Bryan <aliceabryan1996@gmail.com>
2024-02-07 01:46:59 -0500

    strarted work but not completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+
+//Function declaration
+
+razzle("Monika", "the kids")
+function razzle(lawyer = "Billy", target = "'em") {
+    console.log(`${lawyer} razzle-dazzles ${target}!`)
+}
+
+//razzle()
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun()
+
+(funciton () {
+    console.log("I am a function expression")
+});
+
